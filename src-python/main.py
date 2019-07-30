671d98a73337105e5a695d4a1d6e58c01d481a1d
evertoncastro <evertoncastro.sp@gmail.com>
2019-07-30 19:36:49 -0300

    Create a csv

--- src-python/main.py
+++ src-python/main.py
@@ -1,13 +1,33 @@
-
+import names
 
 def start():
 
     print('OK')
 
 
+def create_csv():
+
+    file_with_names = []
+    count = 0
+    while count <= 10:
+        file_with_names.append(f"{count}, {names.get_full_name()}")
+        print(count)
+        count += 1
+
+
+    a = "\n".join(file_with_names)
+
+    with open("hello.csv", "w") as new_csv_file:
+        new_csv_file.write(a)
+        new_csv_file.close()
+
+
+
+
+
 
 if __name__ == '__main__':
-    start()
+    create_csv()
 
 
 
